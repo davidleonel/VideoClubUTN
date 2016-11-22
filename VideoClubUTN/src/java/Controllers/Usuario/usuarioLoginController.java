ab4e2acc8f409d0b3dc2fd9cbe881b0975993931
davidleonel <garcia.david.leonel@gmail.com>
2016-11-22 20:11:51 -0300

    Minor change
    --

--- VideoClubUTN/src/java/Controllers/Usuario/usuarioLoginController.java
+++ VideoClubUTN/src/java/Controllers/Usuario/usuarioLoginController.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -47,7 +47,7 @@ public class usuarioLoginController extends HttpServlet {
                             }
                         } else {
                                     session.setAttribute("mensajeExito", "Usuario no habilitado");
-                                    response.sendRedirect("dasasdasd.jsp");
+                                    response.sendRedirect("index.jsp");
                                 }
                     }else{  
                             session.setAttribute("mensajeExito", "Usuario o contraseña incorrecta.");
